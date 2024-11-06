0bbc11cc6b96914cf8502ac4c5cad7d62729a426
mdparvez6 <mdparvez1466@gmail.com>
2024-11-06 16:47:55 +0530

    added Counter using SetInterval

--- week_01/CounterSetInterval.js
+++ week_01/CounterSetInterval.js
@@ -10,6 +10,6 @@ function abc() {
     a--;
   }
 }
-
+ 
 // clearInterval does not immediately exit the function
 // when a=-1 it executes the remaining function and prints -1 if we dont use else in the above code
